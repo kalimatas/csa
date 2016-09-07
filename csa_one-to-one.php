423203418305ae1ff1bdf563f518e31f314c10b6
Alexander Guz <kalimatas@gmail.com>
2016-09-07 22:22:42 +0200

    Trip reachability bit for one-to-one

--- csa_one-to-one.php
+++ csa_one-to-one.php
@@ -66,127 +66,118 @@ $connections = [
         'from' => 'D',
         'to' => 'E',
         'departure' => 30,
-        'arrival' => 40,
+        'arrival' => 47,
         'trip' => 'T2',
         'change_time' => 4,
     ],
     // T3
     [
         'from' => 'B',
-        'to' => 'C',
-        'departure' => 15,
-        'arrival' => 20,
+        'to' => 'D',
+        'departure' => 25,
+        'arrival' => 35,
+        'trip' => 'T3',
+        'change_time' => 4,
+    ],
+    [
+        'from' => 'D',
+        'to' => 'E',
+        'departure' => 35,
+        'arrival' => 45,
         'trip' => 'T3',
         'change_time' => 4,
     ],
     // T4
     [
-        'from' => 'C',
-        'to' => 'F',
+        'from' => 'B',
+        'to' => 'D',
         'departure' => 25,
-        'arrival' => 26,
+        'arrival' => 32,
+        'trip' => 'T4',
+        'change_time' => 4,
+    ],
+    [
+        'from' => 'D',
+        'to' => 'E',
+        'departure' => 32,
+        'arrival' => 38,
         'trip' => 'T4',
         'change_time' => 4,
     ],
     // T5
-    [
-        'from' => 'H',
-        'to' => 'E',
-        'departure' => 25,
-        'arrival' => 35,
-        'trip' => 'T5',
-        'change_time' => 4,
-    ],
-    [
-        'from' => 'E',
-        'to' => 'I',
-        'departure' => 35,
-        'arrival' => 45,
-        'trip' => 'T5',
-        'change_time' => 5,
-    ],
+//    [
+//        'from' => 'H',
+//        'to' => 'E',
+//        'departure' => 25,
+//        'arrival' => 35,
+//        'trip' => 'T5',
+//        'change_time' => 4,
+//    ],
+//    [
+//        'from' => 'E',
+//        'to' => 'I',
+//        'departure' => 35,
+//        'arrival' => 45,
+//        'trip' => 'T5',
+//        'change_time' => 5,
+//    ],
     // T6
-    [
-        'from' => 'B',
-        'to' => 'E',
-        'departure' => 17,
-        'arrival' => 50,
-        'trip' => 'T6',
-        'change_time' => 4,
-    ],
+//    [
+//        'from' => 'B',
+//        'to' => 'E',
+//        'departure' => 17,
+//        'arrival' => 50,
+//        'trip' => 'T6',
+//        'change_time' => 4,
+//    ],
 ];
 
 // sort by departure
 usort($connections, function ($c1, $c2) {
     return $c1['departure'] - $c2['departure'];
-    //return $c2['departure'] - $c1['departure'];
 });
 
 //print_r($connections); die();
 
-$earliestArrival = [];
-$inConnections = [];
-
-// to save space comment out and check for isset
-foreach ($stops as $stopId => $stop) {
-    $earliestArrival[$stop] = PHP_INT_MAX;
-    $inConnections[$stop] = null;
-}
+$earliestArrival = array_fill_keys(array_values($stops), PHP_INT_MAX);
+$inConnection = array_fill_keys(array_values($stops), null);
+$tripReachability = array_fill_keys(array_values($trips), false);
 
 // input
 $from = 'A';
-$to = 'F';
-$departureTimestamp = -1;
+$to = 'E';
+$departureTimestamp = 5;
 
 // --------------------------
 
 $earliestArrival[$from] = $departureTimestamp;
-$earliestDestinationArrival = PHP_INT_MAX;
 
 foreach ($connections as $cI => $c) {
-    // todo: limit number of IC
-    // todo: profile query
+    $changeTime = false === $tripReachability[$c['trip']]
+        ? $c['change_time']
+        : 0;
 
-    $previousTrip = null;
-    $changeTime = 0;
-
-    $previousConnectionIndex = $inConnections[$c['from']];
-    if ($previousConnectionIndex !== null) {
-        $previousConnection = $connections[$previousConnectionIndex];
-        $previousTrip = $previousConnection['trip'];
-
-        if ($previousTrip !== $c['trip']) {
-            $changeTime = $previousConnection['change_time'];
-        }
-    }
-
-    if ($c['departure'] >= ($earliestArrival[$c['from']] + $changeTime)
-        && $c['arrival'] <= $earliestArrival[$c['to']]
-    ) {
+    if ($c['departure'] >= ($earliestArrival[$c['from']] + $changeTime) && $c['arrival'] < $earliestArrival[$c['to']]) {
+        $tripReachability[$c['trip']] = true;
         $earliestArrival[$c['to']] = $c['arrival'];
-        $inConnections[$c['to']] = $cI;
+        $inConnection[$c['to']] = $cI;
     }
 }
 
 //print_r($earliestArrival);
 
-//if (! isset($inConnections[$to])) {
-if ($inConnections[$to] === null) {
+if ($inConnection[$to] === null) {
     printf("No path from %s to %s at %s\n", $from, $to, $departureTimestamp);
     exit();
 }
 
 // Build path
 $path = [];
-$connectionIndex = $inConnections[$to];
+$connectionIndex = $inConnection[$to];
 while ($connectionIndex !== null) {
     $connection = $connections[$connectionIndex];
     array_unshift($path, $connection);
-    $connectionIndex = $inConnections[$connection['from']];
-
-    //$connectionIndex = isset($inConnections[$connection['from']])
-    //    ? $inConnections[$connection['from']]
-    //    : null;
+    $connectionIndex = $inConnection[$connection['from']];
 }
 
 foreach ($path as $p) {
